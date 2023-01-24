2cb4a4fcce299d0f7f70cb10857bac0ce1097ee4
Presley Peters <presleypeters04@gmail.com>
2023-01-24 16:06:24 +0000

    Added a threshold setting, cleaned up code

--- pcm2pwm.py
+++ pcm2pwm.py
@@ -1,36 +1,42 @@
 import sys
 import os
 
-if len(sys.argv)>2:
+if len(sys.argv)==4:
     file_name=sys.argv[1]
     output_file=sys.argv[2]
+    threshold=sys.argv[3]
     if os.path.exists(file_name):
-        with open(file_name,"rb") as file:
-            file_orig=file.read()
-            
-        file_quantized=[]
-        byte_high=False
-        byte_low_value=120
-        byte_high_value=255-byte_low_value
-        counter=8
-        binary_value=0b10000000
-
-        for byte in file_orig:
-            if byte>byte_high_value or byte<byte_low_value:
-                if byte>byte_high_value:
-                    byte_high=True
-                if byte<byte_low_value:
-                    byte_high=False
-            counter-=1
-            if byte_high:
-                binary_value=binary_value|(1<<counter)
-            else:
-                binary_value=binary_value&~(1<<counter)
-            if counter==0:
-                file_quantized.append(binary_value)
+        if threshold.isdigit():
+            threshold=int(threshold)
+            if threshold>=0 and threshold<=255:
+                with open(file_name,"rb") as file:
+                    file_orig=file.read()
+                    
+                file_quantized=[]
+                byte_high=False
+                byte_low_value=threshold
+                byte_high_value=255-byte_low_value
                 counter=8
-                binary_value=0
-                binary_mask=0b10000000
-            
-        with open(output_file,"wb") as file:
-            file.write(bytearray(file_quantized))
+                binary_value=255
+
+                for byte in file_orig:
+                    if byte>byte_high_value:
+                        byte_high=True
+                    if byte<byte_low_value:
+                        byte_high=False
+                    counter-=1
+                    if not byte_high:
+                        binary_value=binary_value&~(1<<counter)
+                    if counter==0:
+                        file_quantized.append(binary_value)
+                        counter=8
+                        binary_value=255
+                    
+                with open(output_file,"wb") as file:
+                    file.write(bytearray(file_quantized))
+            else:
+                print("Threshold must be between 0 and 255!")
+        else:
+            print("Threshold must be a number!")
+else:
+    print("Input name, output name and threshold required!")
