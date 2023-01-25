a4e4c2596745e890a840e5f1b6e9266ccf93f6b6
Presley Peters <presleypeters04@gmail.com>
2023-01-25 16:29:49 +0000

    Add files via upload

---
+++ pwm2pcm.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+if len(sys.argv)>2:
+    file_name=sys.argv[1]
+    output_file=sys.argv[2]
+    if os.path.exists(file_name):
+        with open(file_name,"rb") as file:
+            file_orig=file.read()
+            
+        file_finished=[]
+        byte_high=False
+
+        for byte in file_orig:
+            byte_bin=bin(byte)[2:]
+            if len(byte_bin)<8:
+                zeroes=""
+                for a in range(0,8-len(byte_bin)):
+                    zeroes+="0"
+                byte_bin=zeroes+byte_bin
+            for bit in byte_bin:
+                file_finished.append(int(bit)*255)
+            
+        with open(output_file,"wb") as file:
+            file.write(bytearray(file_finished))
